4909d1f581bda164269b263ff7eacce13b6e8e8d
GondhiUmaaa <159233195+GondhiUmaaa@users.noreply.github.com>
2024-02-07 22:00:10 +0530

    Create MenuSelection.js

---
+++ MenuSelection.js
@@ -0,0 +1,13 @@
+// MenuSelection.js
+import React from 'react';
+
+const MenuSelection = () => {
+  return (
+    <div className="menuSelection">
+      <h2>Select Your Menu</h2>
+      <p>Choose from our diverse selection of dishes to customize your dining experience.</p>
+    </div>
+  );
+};
+
+export default MenuSelection;
