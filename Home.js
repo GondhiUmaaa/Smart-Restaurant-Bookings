204fe1380c0b75946ace3188ca55d84e525c7ee5
sairam-kottakonda <99779897+sairam-kottakonda@users.noreply.github.com>
2024-02-07 21:50:59 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Our Restaurant</h1>
+      <p>Reserve your table online and enjoy an unforgettable dining experience.</p>
+    </div>
+  );
+};
+
+export default Home;
