5108faecdccf49fc2f63c0ad96207419fb40a07b
sairam-kottakonda <Sairamkottakonda123@gmail.com>
2024-02-21 19:59:48 +0530

    index.js changes

--- smart/src/index.js
+++ smart/src/index.js
@@ -5,7 +5,9 @@ import './assets/styles/global.scss';
 import './assets/styles/main.scss';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
