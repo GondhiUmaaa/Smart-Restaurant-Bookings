a17b5410f640a65b9cd28da12d80b334889d208d
sairam-kottakonda <Sairamkottakonda123@gmail.com>
2024-02-21 21:06:24 +0530

    removed mobile application card in the home page

--- smart/src/pages/Home/Home.jsx
+++ smart/src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import About from "../../components/Home/About/About";
 import Menu from "../../components/Home/Menu/Menu";
-import MobileApp from "../../components/Home/MobileApp/MobileApp";
 import Popular from "../../components/Home/Popular/Popular";
 import Reservation from "../../components/Home/Reservation/Reservation";
 import Slider from "../../components/Home/Slider/Slider";
@@ -19,7 +18,6 @@ const Home = () => {
       <About />
       <Menu />
       <Reservation />
-      <MobileApp />
       <Footer />
     </div>
   );
