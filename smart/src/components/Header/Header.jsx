583455856524f8b94617a8265aff3e08e06f1bb2
sairam-kottakonda <99779897+sairam-kottakonda@users.noreply.github.com>
2024-04-03 19:47:31 +0000

    small changes

--- smart/src/components/Header/Header.jsx
+++ smart/src/components/Header/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
         <div className="logo">
           <img src={DefaultLogo} alt="default-logo" />
         </div>
-        <div className="menu flex flex-row">
+        <div className="menu">
           <nav>
             <ul>
               <li>About Us</li>
